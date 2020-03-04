60b9ad850230f7b3dc30d0021a1bd1e9ea1f58ff
colorful-skill-2236 <chava2793@gmail.com>
2020-03-04 19:40:55 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+git checkout -- spec/introduction_spec.rb
+
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language ( name, language)
+  puts "Hi, my name is #{name} and i am learning to program in #{language}.
+  end"
